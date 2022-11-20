f91fec3632c61050f4a588c31084392e81af645b
Aakash Basnet <iamaakashbasnet@gmail.com>
2022-11-20 20:22:08 +0545

    feat: password change form on settings page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm as BaseUserCreationForm,
+)
 from django.contrib.auth import get_user_model
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 
-from .forms import UserCreationForm, UserUpdateForm
+from .forms import (
+    UserCreationForm,
+    UserUpdateForm,
+)
 
 
 def signup(request):
@@ -24,12 +29,26 @@ def signup(request):
 def settings(request):
     if request.method == 'POST':
         form = UserUpdateForm(request.POST, instance=request.user)
+        pass_form = PasswordChangeForm(user=request.user, data=request.POST)
 
+        # Other fields
         if form.is_valid():
             form.save()
             messages.success(request, 'Your account has been updated.')
             return redirect('home')
+
+        # Password fields
+        if pass_form.is_valid():
+            user = pass_form.save(commit=True)
+            # Logs out user from all session
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Password updated successfully!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Please correct the error below.')
+
     else:
         form = UserUpdateForm(instance=request.user)
+        pass_form = PasswordChangeForm(request.user)
 
-    return render(request, 'accounts/settings.html', {'form': form})
+    return render(request, 'accounts/settings.html', {'form': form, 'pass_form': pass_form})
